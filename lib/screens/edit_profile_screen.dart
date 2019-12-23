619c152edb16357dd06c4ac59ceb594b58d6f7e4
RafaelM <RafaelM.Science@gmail.com>
2019-12-23 16:10:03 -0400

    Feat feed add Users

--- lib/screens/edit_profile_screen.dart
+++ lib/screens/edit_profile_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/models/user_model.dart';
+import 'package:instagram_clone/services/database_service.dart';
 
 class EditProfileScreen extends StatefulWidget {
-
   final User user;
 
   EditProfileScreen({this.user});
@@ -12,6 +12,35 @@ class EditProfileScreen extends StatefulWidget {
 }
 
 class _EditProfileScreenState extends State<EditProfileScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String _name = '';
+  String _bio = '';
+
+  @override
+  void initState(){
+    super.initState();
+    _name = widget.user.name;
+    _bio = widget.user.bio;
+  }
+
+  _submit() {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      //Update user in database
+      String _profilgeImageUrl = '';
+      User user = User(
+        id: widget.user.id,
+        name: _name,
+        profileImageUrl: _profilgeImageUrl,
+        bio: _bio,
+      );
+
+      //database update
+      DatabaseService.updateUser(user);
+      Navigator.pop(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,9 +48,78 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: Text(
-          'Edit Profile'
+          'Edit Profile',
+          style: TextStyle(color: Colors.black),
         ),
       ),
+      body: SingleChildScrollView(
+          child: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: EdgeInsets.all(30.0),
+          child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 60.0,
+                    backgroundImage:
+                        NetworkImage('http://i.redd.ir/dmdqlcdjlwz.jpg'),
+                  ),
+                  FlatButton(
+                    onPressed: () => print('Change profile'),
+                    child: Text(
+                      'Change Profile Image',
+                      style: TextStyle(
+                          color: Theme.of(context).accentColor, fontSize: 16.0),
+                    ),
+                  ),
+                  TextFormField(
+                    initialValue: _name,
+                    style: TextStyle(fontSize: 18.0),
+                    decoration: InputDecoration(
+                        icon: Icon(
+                          Icons.person,
+                          size: 30.0,
+                        ),
+                        labelText: 'Name'),
+                    validator: (input) => input.trim().length < 1
+                        ? 'Please enter a valid name'
+                        : null,
+                    onSaved: (input) => _name = input,
+                  ),
+                  TextFormField(
+                    initialValue: _bio,
+                    style: TextStyle(fontSize: 18.0),
+                    decoration: InputDecoration(
+                        icon: Icon(
+                          Icons.book,
+                          size: 30.0,
+                        ),
+                        labelText: 'Bio'),
+                    validator: (input) => input.trim().length > 150
+                        ? 'Please enter a bio less than 150 characters'
+                        : null,
+                    onSaved: (input) => _bio = input,
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(40.0),
+                    height: 40.0,
+                    width: 250.0,
+                    child: FlatButton(
+                      onPressed: _submit,
+                      color: Colors.blue,
+                      textColor: Colors.white,
+                      child: Text(
+                        'Save Profile',
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                    ),
+                  ),
+                ],
+              )),
+        ),
+      )),
     );
   }
 }
