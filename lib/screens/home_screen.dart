2fb09dba4cbec5533df6f4a759f1eccf88314cc6
RafaelM <RafaelM.Science@gmail.com>
2019-12-27 16:09:29 -0400

    Feat: Search profile
    - add search profile

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -30,16 +30,17 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: Text(
-            "Instagram",
-            style: TextStyle(
-              color: Colors.black,
-              fontFamily: 'Billabong',
-              fontSize: 35.0,
-            ),
-          )),
+//      caso queria o AppBar em todas
+//      appBar: AppBar(
+//          backgroundColor: Colors.white,
+//          title: Text(
+//            "Instagram",
+//            style: TextStyle(
+//              color: Colors.black,
+//              fontFamily: 'Billabong',
+//              fontSize: 35.0,
+//            ),
+//          )),
       body: PageView(
         controller: _pageController,
         children: <Widget>[
