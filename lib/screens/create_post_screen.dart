c39e5fa69f50efaf9b6920cce28d70d825ed72e9
RafaelM <RafaelM.Science@gmail.com>
2019-12-30 16:51:08 -0400

    Feat: Create Post
    -add icon camera
    -add icon plus

--- lib/screens/create_post_screen.dart
+++ lib/screens/create_post_screen.dart
@@ -1,3 +1,6 @@
+
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class CreatePostScreen extends StatefulWidget {
@@ -6,23 +9,57 @@ class CreatePostScreen extends StatefulWidget {
 }
 
 class _CreatePostScreenState extends State<CreatePostScreen> {
+
+  _showSelectImageDialog(){
+    return Platform.isIOS ? _iosBottomSheet() : _androidDialog();
+  }
+
+  _iosBottomSheet(){
+    print('ios');
+  }
+
+  _androidDialog(){
+    print('android');
+  }
+
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: Text(
-            "Instagram",
-            style: TextStyle(
-              color: Colors.black,
-              fontFamily: 'Billabong',
-              fontSize: 35.0,
-            ),
-          )),
-      body: Center(
-        child: Text(
-          'CreatePost Screen',
+        backgroundColor: Colors.white,
+        title: Text(
+          "Create Post",
+          style: TextStyle(
+            color: Colors.black,
+            fontFamily: 'Billabong',
+            fontSize: 35.0,
+          ),
         ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => print('Add'),
+          )
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          GestureDetector(
+            onTap: _showSelectImageDialog,
+            child: Container(
+              height: width,
+              width: width,
+              color: Colors.grey[300],
+              child: Icon(
+                Icons.add_a_photo,
+                color: Colors.white70,
+                size: 150.0,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
